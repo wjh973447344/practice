5a166895575e3660462de9d47b86a69d12fb67fc
wjh973447344 <54193487+wjh973447344@users.noreply.github.com>
2022-09-16 14:48:17 +0800

    新增测试1

---
+++ src/main/java/com/example/practice/test/test1.java
@@ -0,0 +1,10 @@
+package com.example.practice.test;
+
+public class test1 {
+    public static void main(String[] args) {
+
+    }
+    public static int resultAdd(int add1, int add2){
+        return add1 + add2;
+    }
+}
